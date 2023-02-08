897183df38c40d3ef09f5324e5803903d09d1a69
Joshua Kawamata <email>
2023-02-08 19:26:07 +0900

    [refactor][test]: variable refactor, comment changes , test for optional coupon

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { app } from '../app';
 
-
+const endpointRequest = request.agent(app);
 
 describe('GET "/items" アイテム一覧test', () => {
     const cartData = [
@@ -11,7 +11,7 @@ describe('GET "/items" アイテム一覧test', () => {
     ];
 
     it('responseとアイテム一覧が一致している', async () => {
-        const res = await request(app).get('/items');
+        const res = await endpointRequest.get('/items');
         expect(res.body).toEqual(cartData);
     });
 });
@@ -20,7 +20,7 @@ describe('POST /carts/calculate カート内容金額計算', () => {
     it('ディスカウントなしでカートの金額の合計が返ってくる', async () => {
         const cartItems = { itemIds: [1, 2, 3], coupon: 'X' };
 
-        const res = await request(app)
+        const res = await endpointRequest
             .post('/carts/calculate')
             .send(cartItems)
             .set('content-type', 'application/json');
@@ -30,26 +30,37 @@ describe('POST /carts/calculate カート内容金額計算', () => {
     it('ディスカウントありで割り引いた金額が返ってくる', async () => {
         const cartItems = { itemIds: [1, 2, 3], coupon: 'XXXXX' };
 
-        const res = await request(app)
+        const res = await endpointRequest
             .post('/carts/calculate')
             .send(cartItems)
             .set('content-type', 'application/json');
 
         expect(res.body.totalPrice).toEqual(540);
     });
+    it('クーポンが指定されていない場合、割り引いていない合計金額が返ってくる', async () => {
+        // item idのみのheaders
+        const cartItems = { itemIds: [1, 2, 3] };
+
+        const res = await endpointRequest
+            .post('/carts/calculate')
+            .send(cartItems)
+            .set('content-type', 'application/json');
+
+        expect(res.body.totalPrice).toEqual(600);
+    });
 });
 
 describe('invalid テストケース', () => {
     it('GET 存在していないエンドポイント 404 返す', async () => {
         //存在しないエンドポイント
-        const res = await request(app).get('/ite');
+        const res = await endpointRequest.get('/ite');
         expect(res.statusCode).toEqual(404);
     });
     it('存在しないアイテムを計算すると、statuscode 400とメッセージが返ってくる ', async () => {
-        // id 一個多く
+        // item id 一個多く
         const cartItems = { itemIds: [1, 2, 3, 4], coupon: 'XXXXX' };
 
-        const res = await request(app)
+        const res = await endpointRequest
             .post('/carts/calculate')
             .send(cartItems)
             .set('content-type', 'application/json');
